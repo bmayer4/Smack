45310ef630eac79da6876ccaa0fc6c748cb6987b
bmayer4 <bmayer55@yahoo.com>
2017-09-01 23:41:10 -0400

    set up create acct button logic

--- Smack/CreateAccountVC.swift
+++ Smack/CreateAccountVC.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,5 +25,29 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
 
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else {
+            return
+        }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered user!")
+            } else {
+                
+            }
 
+        }
+        
+    }
+
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
 }
---
+++ Smack/RoundedButton.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Brett Mayer on 9/1/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    //if I comment this out you can still see the changes in storyboard...?
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
