ebcd79768c1b814aee5bdaf7f33379f50c7c1099
bmayer4 <bmayer55@yahoo.com>
2017-09-03 15:23:54 -0400

    completed create acct vc and added random color avatars, and notification

---
+++ Smack/CircleImage.swift
@@ -0,0 +1,31 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Brett Mayer on 9/3/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+
+}
